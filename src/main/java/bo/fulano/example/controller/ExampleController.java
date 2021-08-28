025c6d5da60947eabde7221197c00ea737725078
dtarqui <macuto2142dt@gmail.com>
2021-08-27 21:56:23 -0400

    JP-36 testing again

---
+++ src/main/java/bo/fulano/example/controller/ExampleController.java
@@ -0,0 +1,14 @@
+package bo.fulano.example.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ExampleController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getAssuranceByUserId() {
+        return "Hello World";
+    }
+}
