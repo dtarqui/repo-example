eb0f505afb8f652abfa298a5323b5ae57b38d5a7
dtarqui <macuto2142dt@gmail.com>
2021-08-27 22:22:48 -0400

    JP-36 testing java 11

--- src/main/java/bo/fulano/example/controller/ExampleController.java
+++ src/main/java/bo/fulano/example/controller/ExampleController.java
@@ -3,12 +3,14 @@ package bo.fulano.example.controller;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
+
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ExampleController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
     public String getAssuranceByUserId() {
-        return "Hello World";
+        return "Hello World. Now It's " + Instant.now().toString();
     }
 }
